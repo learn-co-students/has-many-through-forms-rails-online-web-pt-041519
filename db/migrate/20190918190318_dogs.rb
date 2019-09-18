769f296708beebd7fb6b709cddfbc401c984a325
Cody <cody.frank30@gmail.com>
2019-09-18 15:07:30 -0400

    Done.

---
+++ db/migrate/20190918190318_dogs.rb
@@ -0,0 +1,4 @@
+class Dogs < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160115202458) do
+ActiveRecord::Schema.define(version: 20190918190354) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "owners", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -24,10 +28,9 @@ ActiveRecord::Schema.define(version: 20160115202458) do
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_post_categories_on_category_id"
+    t.index ["post_id"], name: "index_post_categories_on_post_id"
   end
-
-  add_index "post_categories", ["category_id"], name: "index_post_categories_on_category_id"
-  add_index "post_categories", ["post_id"], name: "index_post_categories_on_post_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
